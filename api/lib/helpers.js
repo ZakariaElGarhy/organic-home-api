3db0d27435d9f242667a8726e3c9177538db5a89
xLeeBro <168808049+ZakariaElGarhy@users.noreply.github.com>
2026-09-25 19:11:24 +0300

    Add files via upload

--- api/lib/helpers.js
+++ api/lib/helpers.js
@@ -22,7 +22,7 @@ function applyCors(req, res) {
     res.setHeader('Access-Control-Allow-Origin', origin);
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE,OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, X-Filename');
   }
 }
 
